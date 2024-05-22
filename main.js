94e89e6bbcb04fa5051791336579931cc4486ca4
deweyda334 <153093259+deweyda334@users.noreply.github.com>
2024-05-22 14:09:23 -0400

    Added project files to the repo

---
+++ main.js
@@ -0,0 +1,10 @@
+// Dax Dewey
+// 22 MAY 2024
+// JS Landing Page
+
+let firstName = prompt("What is your name?");
+let focus = prompt("What is your main focus for today?");
+
+// Output Data into the Page
+document.getElementById("nameSpan").innerHTML = firstName;
+document.getElementById("focusP").innerHTML = focus;
